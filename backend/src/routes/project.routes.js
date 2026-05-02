838934e76c6f8678a80e4a1b848fa55aac073871
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-05-02 23:05:58 +0530

    added delete update and get a single project by id api

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -2,14 +2,18 @@ import express from "express";
 import { verifyUser, verifyAdmin } from "../middlewares/verifyuser.js";
 import {
   createProject,
-  getProjects,
   addMembersToProject,
+  getAllProjects,
+  getProjectById,
+  deleteProject,
 } from "../controllers/project.controller.js";
 
 const projectRoutes = express.Router();
 
 projectRoutes.post("/create", verifyUser, verifyAdmin, createProject);
-projectRoutes.get("/getprojects", verifyUser, getProjects);
+projectRoutes.get("/getprojects", verifyUser, getAllProjects);
+projectRoutes.get("/:projectId", verifyUser, getProjectById);
+projectRoutes.delete("/:projectId", verifyUser, verifyAdmin, deleteProject);
 projectRoutes.post(
   "/add-members",
   verifyUser,
